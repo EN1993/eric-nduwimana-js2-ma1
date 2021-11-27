2ac2b14f926c4b443b504c05b60e111d4071e415
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-11-27 16:22:14 +0100

    add

---
+++ JS/favourite.js
@@ -0,0 +1,19 @@
+import {getExistingFavs} from"./utils/favFunctions.js";
+
+const favourite = getExistingFavs();
+
+const productContainer = document.querySelector(".container");
+
+if (favourite.length === 0) {
+  productContainer.innerHTML =" No Favourites Yet";
+}
+
+favourite.forEach((favourites) => {
+  productContainer.innerHTML += `<div class="product" 
+  
+                                  <h4>Title: ${favourites.title}</h4>
+                                  <p>Price: $${favourites.price}</p>
+                                  <i class="fa fa-heart"></i>
+  
+                                  </div>`
+});
--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
-
+import {getExistingFavs} from"./utils/favFunctions.js";
 const productContainer = document.querySelector(".container");
 const search = document.querySelector(".search");
+
+const favourite = getExistingFavs();
 
 const url="https://fakestoreapi.com/products";
 
@@ -14,33 +16,96 @@ async function productsItem() {
   
 
   product.forEach(function (item) {
+
+    let cssClass="far";
+
+    const doesObjectExist = favourite.find(function (fav) {
+      console.log(fav);
+      return parseInt(fav.id) === item.id;
+    });
+
+    console.log(doesObjectExist);
+
+    if (doesObjectExist) {
+      cssClass="fa";
+    }
+
+
+
+
+
     productContainer.innerHTML += `<div class="product">
     
                                       <h4>Title: ${item.title}</>
                                       <p>Price: $${item.price}</p>
+                                      <i class="${cssClass} fa-heart" data-id="${item.id}" data-title="${item.title}" data-price="${item.price}"></i>
     
     </div>`;
 });
 
-}
+const favButtons = document.querySelectorAll(".product i");
 
-productsItem();
-
-search.onkeyup = function (event) {
-  
-console.log(event);
-
-const searchValue = event.target.value.trim()
-const filteredProduct = product.filter(function(item) {
-  if (item.price <= (searchValue)) {
-    return true;
-  }
+favButtons.forEach((button) => {
+    button.addEventListener("click", handleClick);
 });
 
-product = filteredProduct;
+function handleClick() {
+    this.classList.toggle("fa");
+    this.classList.toggle("far");
+
+    const id = this.dataset.id;
+    const title = this.dataset.title;
+    const price = this.dataset.price;
+    
+    const currentFavs = getExistingFavs();
+
+    const itemExists= currentFavs.find(function (fav) {
+      return fav.id === id;
+    });
+
+    if (itemExists === undefined) {
+      const item = { id: id, title: title, price: price };
+      currentFavs.push(item);
+      saveFavs(currentFavs);
+    } else {
+      const newFavs = currentFavs.filter((fav) => fav.id != id);
+      saveFavs(newFavs);
+    }
+
+    
+
+ }
+
+ 
+
+ function saveFavs(favs) {
+   localStorage.setItem("favourite", JSON.stringify(favs));
+ }
+
+}
 
 productsItem();
-}
+
+
+
+
+// search.onkeyup = function (event) {
+  
+// // console.log(event);
+
+// const searchValue = event.target.value.trim()
+// const filteredProduct = product.filter(function(item) {
+//   if (item.price <= (searchValue)) {
+//     return true;
+//   }
+// });
+
+
+// product = filteredProduct;
+// productsItem();
+
+// }
+
 
 
  
